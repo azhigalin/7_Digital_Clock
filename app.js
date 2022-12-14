5f7e4a6483f96190a5195b966d6b77e7365771f0
Aleksandr <azhigalin20@gmail.com>
2022-12-14 23:38:50 +0300

    Commit 1
    Загрузка файлов проекта

---
+++ app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", calcDate);
+
+function calcDate() {
+  let date = new Date();
+  let dayNumber = date.getDay();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let ampm = hour >= 12 ? "PM" : "AM";
+  let dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  hour = hour % 12;
+  hour = hour ? 12 : "12";
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+
+  document.querySelector("#day").textContent = dayNames[dayNumber];
+  document.querySelector("#hour").textContent = hour;
+  document.querySelector("#minute").textContent = minute;
+  document.querySelector("#ampm").textContent = ampm;
+}
+
+setInterval(calcDate, 500);
